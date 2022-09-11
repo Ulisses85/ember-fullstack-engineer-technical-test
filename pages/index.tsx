b788ac681e1e0faea0881d11b1798e90508666bc
“lukasz-pill” <lukasz.kuligowski@truepill.com>
2022-09-11 16:57:31 +0200

    add server side fetching in index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,31 @@
+import { GetServerSideProps } from "next";
 import Head from "next/head";
 import WeatherCard from "../components/WeatherCard";
 
-export default function Home() {
+export default function Home({ data }: any) {
   return (
     <>
       <Head>
         <title>Weather Card App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <WeatherCard />
+      <WeatherCard {...data} />
     </>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const res = await fetch(`http://localhost:3000/api/weather`, {
+    method: "GET",
+  });
+  const data = await res.json();
+
+  if (!data) {
+    return {
+      notFound: true,
+    };
+  }
+  return {
+    props: { data },
+  };
+};
