928699dcec4ea7986710ad1039576bb9dbfe6387
“lukasz-pill” <lukasz.kuligowski@truepill.com>
2022-09-11 16:58:03 +0200

    changed some global styles

--- components/globalstyles.tsx
+++ components/globalstyles.tsx
@@ -3,9 +3,12 @@ import { createGlobalStyle } from "styled-components";
 const GlobalStyle = createGlobalStyle`
   html,
   body {
+    background-color: black;
     color: ${({ theme }) => theme.colors.primary};
     padding: 0;
     margin: 0;
+    display:flex;
+    justify-content: center;
     font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
       Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
   }
